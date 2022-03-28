0598e1f9c0bba8cfb40d8b571d5c362e29d9470c
krypton225 <k.mohamed.11.98@gmail.com>
2022-03-28 21:01:13 +0200

    ADD click (e) to button

--- js/script.js
+++ js/script.js
@@ -9,8 +9,35 @@ let students = [
 ];
 
 // When load the window, print in the screen all data name of the students[].
+function designUI(stdsNames) {
+  students.forEach(function (items) {
+    containerBody.innerHTML += `${items.studentName}. <br>`;
+  });
+}
+
 window.onload = () => {
-  for (let i = 0, j = 1; i < students.length; i++, j++) {
-    containerBody.innerHTML += `${j}- ${students[i].studentName} <br>`;
-  }
+  designUI(students);
 };
+
+console.log(students);
+
+// When input is recived a student name.
+mySubmit.addEventListener("click", () => {
+  if (myInput.value == "") {
+    alert("Please, Enter the student name!");
+  } else {
+    let lastID = students.length ? students[students.length - 1].id : 1;
+
+    students.push({
+      id: ++lastID,
+      studentName: myInput.value,
+      studentClass: "1/2",
+    });
+
+    containerBody.innerHTML += `${
+      students[students.length - 1].studentName
+    } <br>`;
+
+    myInput.value = "";
+  }
+});
